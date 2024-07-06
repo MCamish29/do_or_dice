0e9c441beb2e495ec67df60990bca783659f7795
MCamish29 <mcamish29@gmail.com>
2024-07-06 16:08:42 +0100

    "Refine JS script for dice process"

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,15 +19,82 @@ let rollD4 = document.getElementById("roll6");
 /**Adding DOM content loaded and event listners for buttons */
 document.addEventListener("DOMContentLoaded", function(){
     buttons = document.getElementsByTagName("button");
+
 });
 
+//Dice roll object of key value pairs that holds the multipoliers of each dice
+let diceRoll = {
+    "d20": 20,
+    "d12": 12,
+    "d10": 10,
+    "d8": 8,
+    "d6": 6,
+    "d4": 4,
+}
 
-let d20Result = () => {
-    return Math.floor(Math.random() *20)+1;
-};
+/**
+ * 
+ * This function takes a string of the dice roll you want
+ * it then sets the multiplier based on the input by checking the diceRollobject
+ * @param {string} dice
+ * @returns integer
+ */
+
+function rollDice(dice){
+    let multiplier =1;
+    if(dice === "d20"){
+        multiplier = diceRoll.d20;    
+    }
+    else if (dice = "d12"){
+        multiplier = diceRoll.d12;
+    }
+    else if (dice = "d10"){
+        multiplier = diceRoll.d10;
+    }
+    else if (dice = "d8"){
+        multiplier = diceRoll.d8;
+    }
+    else if (dice = "d6"){
+        multiplier = diceRoll.d6;
+    }
+        else if (dice = "d4"){
+        multiplier = diceRoll.d4;
+    }
+    return Math.floor(Math.random() * multiplier) +1;
+}
+
+
 rollD20.addEventListener("click",() => {
-    result1.innerHTML = d20Result();
+    let result =rollDice("d20");
+    result1.innerHTML = result;
 });
+rollD12.addEventListener("click",() => {
+    let result =rollDice("d12");
+    result2.innerHTML = result;
+});
+rollD10.addEventListener("click",() => {
+    let result =rollDice("d10");
+    result3.innerHTML = result;
+});
+
+rollD8.addEventListener("click",() => {
+    let result =rollDice("d8");
+    result4.innerHTML = result;
+});
+
+rollD6.addEventListener("click",() => {
+    let result =rollDice("d6");
+    result5.innerHTML = result;
+});
+rollD4.addEventListener("click",() => {
+    let result =rollDice("d4");
+    result6.innerHTML = result;
+});
+
+
+
+
+
 
 let d12Result = () => {
     return Math.floor(Math.random() *12)+1;
